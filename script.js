5428a9c5280bd09cb57da26501e40cfd087fd3b8
jazzxsyy1 <148664432+jazzxsyy1@users.noreply.github.com>
2024-02-13 22:35:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.ATTRIBUTE_NODE.innerHTML = "YAYY! I love you too sososo much! mwaaa!";
+    gif.src =
+    "https://images.app.goo.gl/4bKiQGCUBGDKeJZc6"
+
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+})
